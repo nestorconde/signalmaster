a01384ee8a56637d348220ed7c48880db3eeb6e8
Jacob Dharandas Méndez <j.dharandas.mendez@learnvolution.com>
2019-10-06 12:04:17 +0200

    Added express

--- server.js
+++ server.js
@@ -3,34 +3,29 @@ var yetify = require('yetify'),
     config = require('getconfig'),
     fs = require('fs'),
     sockets = require('./sockets'),
+    express = require('express'),
     port = parseInt(process.env.PORT || config.server.port, 10),
-    server_handler = function (req, res) {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        if (req.url === '/healthcheck') {
-            console.log(Date.now(), 'healthcheck');
-            res.writeHead(200);
-            res.end();
-            return;
-        }
-        res.writeHead(404);
-        res.end();
-    },
-    server = null;
+    server = null
+    server_handler = express()
+;
 
+server_handler.get('/health-check', function (req, res) {
+    console.log(Date.now(), 'healthcheck');
+    return res.sendStatus(200);
+});
+server_handler.use(express.static('static'));
 
 // Create an http(s) server instance to that socket.io can listen to
 if (config.server.secure) {
-    server = require('https').Server({
+    server = require('https').createServer({
         key: fs.readFileSync(config.server.key),
         cert: fs.readFileSync(config.server.cert),
         passphrase: config.server.password
     }, server_handler);
 } else {
-    server = require('http').Server(server_handler);
+    server = require('http').createServer(server_handler);
 }
 server.listen(port);
-
 sockets(server, config);
 
 if (config.uid) process.setuid(config.uid);
